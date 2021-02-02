552de3aa13cf01e1bace1018e9f0fda4d7163b2f
zscheinfeld <zachscheinfeld@Zachs-MacBook-Pro-2.local>
2021-02-02 08:38:59 -0500

    Website Draft 01.02.21

--- Final Draft/script.js
+++ Final Draft/script.js
@@ -13,19 +13,19 @@ $("document").ready(function() {
 
 //variables// 
 
-var $inner = $(".flexbox-item-client-container");
-var $grid = $(".flexbox-item-client-container");
-var wh = window.innerHeight;
-var ww = window.innerWidth;
-var inner_sw = $inner.scrollWidth;
-var topMax = $inner.scrollWidth - ww;
-var scrollwidth;
+// var $inner = $(".flexbox-item-client-container");
+// var $grid = $(".flexbox-item-client-container");
+// var wh = window.innerHeight;
+// var ww = window.innerWidth;
+// var inner_sw = $inner.scrollWidth;
+// var topMax = $inner.scrollWidth - ww;
+// var scrollwidth;
 
 
-console.log(inner_sw)
-console.log(topMax)
-console.log(wh)
-console.log(ww)
+// console.log(inner_sw)
+// console.log(topMax)
+// console.log(wh)
+// console.log(ww)
 
 
 // var onscroll = (e) => {
@@ -35,3 +35,27 @@ console.log(ww)
 //     $inner[0].scrollLeft = -__top;
 //     console.log(top);
 //     };
+
+
+$(document).ready(function(){
+    $('.client-hero-images').on('click', function(){
+        console.log('clicked')
+    });
+});
+
+
+//load
+// window.onload = function()
+// {
+//     document.getElementById("home").scrollIntoView();
+// };
+
+
+window.addEventListener("scroll", function(event){
+    var scroll =this.scrollY;
+    console.log(scroll)
+    const scrollable=document.documentElement.scrollHeight - window.innerHeight
+    // const scrolled = window.scrollY;
+    console.log(scrollable);
+});
+
