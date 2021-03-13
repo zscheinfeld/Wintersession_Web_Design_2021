ecdae9a87a218966a8a5d2eee754387db412a805
zscheinfeld <zachscheinfeld@Zachs-MacBook-Pro-2.local>
2021-03-13 18:33:20 -0500

    03.12.21

--- Final_Draft_02_09_21/script.js
+++ Final_Draft_02_09_21/script.js
@@ -1,69 +1,127 @@
+
+
 window.scrollTo(0, 5000);
 
-    // scroll 
-
-
 $("document").ready(function() {
-    var carouselSlide = document.querySelector("#critical");
-    var carouselImages = document.querySelectorAll("#critical img");
-    var countercritical = 1;
-    var size = carouselImages[0].clientWidth;
-    var carouselSlideGroji = document.querySelector("#groji");
-    var carouselImagesGroji = document.querySelectorAll("#groji img");
-    var grojicounter = 1;
-    var carouselSlideOG = document.querySelector("#OG");
-    var carouselImagesOG = document.getElementsByClassName("OG-Slides");
-    var OGcounter = 1;
-    var OGvideos = document.getElementsByClassName("isOGvideo");
     const landing = document.getElementById('landing')
     var studiosectionwidth = $(".flexbox-item-studio").width() + 921 + 7;
     window.scrollTo(0, studiosectionwidth);
-    console.log("load");
-    carouselSlideGroji.style.transition = "none";
-    carouselSlideGroji.style.transform= 'translateX(' +(-size * grojicounter) + 'px)';
-    carouselSlide.style.transition = "none";
-    carouselSlide.style.transform= 'translateX(' +(-size * countercritical) + 'px)';
-    carouselSlideOG.style.transition = "none";
-    carouselSlideOG.style.transform= 'translateX(' +(-size * countercritical) + 'px)';
-    console.log(size + "size")
+    console.log(studiosectionwidth + "scroll width");
+});
 
-    window.addEventListener("scroll", function(event){
-        var studiosectionwidth = $(".flexbox-item-studio").width() + 921 + 7;
-        var clientsectionwidth = $(".flexbox-item-client-container").width() + 921;
-        $(".flexbox-item-transparent-client").height(clientsectionwidth);
-        $(".flexbox-item-transparent").height(studiosectionwidth);
-        var scroll =this.scrollY;
-        console.log(scroll)
-        console.log(studiosectionwidth + "sectionstudiowidth")
-        var scrollable = document.documentElement.scrollHeight - window.innerHeight
-    
-    
-        if(scroll < (studiosectionwidth)){
-            scrollable = scrollable + window.innerHeight
-            console.log('when client container is hidden' + 'scrollable is' + scrollable)
-            var left = $(".flexbox-item-studio").css("left")
-            console.log(left + "left")
-            console.log(scroll)
-            $(".flexbox-item-client-container").hide();
-            $(".flexbox-item-studio").show();
-            $(".flexbox-item-studio").css("left" , (- (1*window.scrollY)) + "px");
-        
+    // critical button
+
+    var criticalbuttoncounter = 1
+    $("#buttoncritical").on('click', function(){
+        console.log("critical")
+        if (criticalbuttoncounter % 2==0) {
+        $(".project-information-critical").removeClass("opacity-off-start");
+        $(".project-information-critical").removeClass("opacity-on");
+        $(".project-information-critical").addClass("opacity-off");
+        $("#criticalbutton").addClass("buttonminus")
+        $("#criticalbutton").removeClass("buttonplus")
+        console.log('minus')
+        criticalbuttoncounter = criticalbuttoncounter + 1
+        console.log(criticalbuttoncounter)
+        function moveback(){
+            $(".project-information-critical").removeClass("move-up")
+            $(".project-information-critical").addClass("move-back")
+        }
+        setTimeout(moveback,500);
         }
         else {
-            console.log('when client container is shown' + 'scrollable is' + scrollable)
-            $(".flexbox-item-client-container").show();
-            $(".flexbox-item-studio").css("visibilty" , "hidden");
-            $(".flexbox-item-studio").css("left" , (studiosectionwidth));
-            $(".flexbox-item-client-container").css("left" , (- (1*(window.scrollY-(studiosectionwidth + (window.innerHeight/2)))) + "px"));
-            
+        $(".project-information-critical").addClass("opacity-on");
+        $(".project-information-critical").removeClass("opacity-off");
+        $("#criticalbutton").addClass("buttonplus")
+        $("#criticalbutton").removeClass("buttonminus")
+        console.log('plus')
+        criticalbuttoncounter = criticalbuttoncounter + 1
+        console.log(criticalbuttoncounter)
+        
+        function moveup(){
+            $(".project-information-critical").removeClass("move-back")
+            $(".project-information-critical").addClass("move-up")
         }
-    });
+        moveup(); 
+        } 
+        });
+
+        // groji button // 
+
+         // critical button
+
+    var grojibuttoncounter = 1
+    $("#buttongroji").on('click', function(){
+        console.log("groji")
+        if (grojibuttoncounter % 2==0) {
+        $(".project-information-groji").removeClass("opacity-off-start");
+        $(".project-information-groji").removeClass("opacity-on");
+        $(".project-information-groji").addClass("opacity-off");
+        $("#grojibutton").addClass("buttonminus")
+        $("#grojibutton").removeClass("buttonplus")
+        console.log('minus')
+        grojibuttoncounter = grojibuttoncounter + 1
+        console.log(criticalbuttoncounter)
+        function moveback(){
+            $(".project-information-groji").removeClass("move-up")
+            $(".project-information-groji").addClass("move-back")
+        }
+        setTimeout(moveback,500);
+        }
+        else {
+        $(".project-information-groji").addClass("opacity-on");
+        $(".project-information-groji").removeClass("opacity-off");
+        $("#grojibutton").addClass("buttonplus")
+        $("#grojibutton").removeClass("buttonminus")
+        console.log('plus')
+        grojibuttoncounter = grojibuttoncounter + 1
+        console.log(grojibuttoncounter)
+        
+        function moveup(){
+            $(".project-information-groji").removeClass("move-back")
+            $(".project-information-groji").addClass("move-up")
+        }
+        moveup(); 
+        } 
+        });
+
     
 
+    // slideshows
 
-    $("#groji").on('click', function(event){
-        if (grojicounter >= carouselImagesGroji.length-1) return;
+    // critical 39 slideshow
+
+    const carouselSlide = document.querySelector("#critical");
+    const carouselImages = document.querySelectorAll("#critical img");
+    var countercritical = 1;
+    var size = carouselImages[0].clientWidth;
+    carouselSlide.style.transform = 'translateX(' +(-size * countercritical) + 'px)';
+    $("#critical").on('click', function(){
+        if (countercritical >= carouselImages.length-1) return;
         console.log('clicked')
+        carouselSlide.style.transition = "transform 1s ease-in-out";
+        countercritical++;
+        carouselSlide.style.transform= 'translateX(' +(-size * countercritical) + 'px)';
+    });
+
+    carouselSlide.addEventListener('transitionend', ()=>{
+        if (carouselImages[countercritical].id === 'firstClonecritical') {
+                carouselSlide.style.transition = "none";
+                countercritical = carouselImages.length - countercritical;
+                carouselSlide.style.transform = 'translateX(' +(-size * countercritical) + 'px)';}
+    
+    });
+
+    // Groji slideshow
+
+    const carouselSlideGroji = document.querySelector("#groji");
+    const carouselImagesGroji = document.querySelectorAll("#groji img");
+    var grojicounter = 1;
+    // var size = carouselImagesGroji[0].clientWidth;
+
+    carouselSlideGroji.style.transform = 'translateX(' +(-size * grojicounter) + 'px)';
+    $("#groji").on('click', function(){
+        if (grojicounter >= carouselImagesGroji.length-1) return;
         carouselSlideGroji.style.transition = "transform 1s ease-in-out";
         grojicounter++;
         carouselSlideGroji.style.transform= 'translateX(' +(-size * grojicounter) + 'px)';
@@ -73,136 +131,141 @@ $("document").ready(function() {
         if (carouselImagesGroji[grojicounter].id === 'firstClonegroji') {
                 carouselSlideGroji.style.transition = "none";
                 grojicounter = carouselImagesGroji.length - grojicounter;
-                carouselSlideGroji.style.transform = 'translateX(' +(-size * grojicounter) + 'px)';}
+                carouselSlideGroji.style.transform = 'translateX(' +(-size * grojicounter) + 'px)';
+            }
+    
     });
 
-    //resize
+    // OG slideshow
+
+    const carouselSlideOG = document.querySelector("#OG");
+    // const carouselImagesOG = document.querySelectorAll("#OG img  video");
+    const carouselImagesOG = document.getElementsByClassName("OG-Slides");
+    var OGcounter = 1;
+
+    const OGvideos = document.getElementsByClassName("isOGvideo");
+
+    carouselSlideOG.style.transform = 'translateX(' +(-size * OGcounter) + 'px)';
+
+    $("#OG").on('click', function(){
+      
+        if (OGcounter >= carouselImagesOG.length-1) return;
+            carouselSlideOG.style.transition = "transform 1s ease-in-out";
+            OGcounter++;
+            carouselSlideOG.style.transform= 'translateX(' +(-size * OGcounter) + 'px)';
+
+    //    if (OGcounter = 1)
+    //         console.log(OGvideos)
+        
+
+        // if (OGcounter !== 1); 
+        //     // OGvideos.pause()
+        //     // OGvideos.currentTime=0;
+        //     // OGvideos.load();
+        //     console.log("pause")
+         
+    });
+
+
+        carouselSlideOG.addEventListener('transitionend', ()=>{
+        if (carouselImagesOG[OGcounter].id === 'firstCloneog') {
+            carouselSlideOG.style.transition = "none";
+            OGcounter = carouselImagesOG.length - OGcounter;
+            carouselSlideOG.style.transform = 'translateX(' +(-size * OGcounter) + 'px)';}
+
+    
+
+    });
+
+    // Information Button 
+
+    
+
+
+
+
+
+    
+
+    //  // covid slideshow
+
+    //  const carouselSlidecovid = document.querySelector("#covid");
+    //  const carouselImagescovid = document.querySelectorAll("#covid img");
+    //  var covidcounter = 1;
+ 
+    //  carouselSlidecovid.style.transform = 'translateX(' +(-size * covidcounter) + 'px)';
+ 
+    //  $("#covid").on('click', function(){
+    //      if (covidcounter >= carouselImagescovid.length-1) return;
+    //          carouselSlidecovid.style.transition = "transform 1s ease-in-out";
+    //          covidcounter++;
+    //          carouselSlidecovid.style.transform= 'translateX(' +(-size * covidcounter) + 'px)';
+    //  });
+ 
+    //  carouselSlidecovid.addEventListener('transitionend', ()=>{
+    //      if (carouselImagescovid[covidcounter].id === 'firstClonecovid') {
+    //          carouselSlidecovid.style.transition = "none";
+    //          covidcounter = carouselImagescovid.length - covidcounter;
+    //          carouselSlidecovid.style.transform = 'translateX(' +(-size * covidcounter) + 'px)';}
+ 
+    //  });
+
+    // resize
 
     window.onresize = function(event) {
         size = carouselImages[countercritical].clientWidth;
         // size = carouselImagesGroji[counter].clientWidth;
         carouselSlideGroji.style.transition = "none";
-        carouselSlideGroji.style.transform= 'translateX(' + (-size * grojicounter) + 'px)';
+        carouselSlideGroji.style.transform= 'translateX(' +(-size * grojicounter) + 'px)';
         // size = carouselImages[countercritical].clientWidth;
         carouselSlide.style.transition = "none";
         carouselSlide.style.transform= 'translateX(' +(-size * countercritical) + 'px)';
         // size = carouselImagesOG[OGcounter].clientWidth;
         carouselSlideOG.style.transition = "none";
         carouselSlideOG.style.transform= 'translateX(' +(-size * countercritical) + 'px)';
-
+    
     };
 
 
-        // critical button
-
-        var criticalbuttoncounter = 1
-        $("#buttoncritical").on('click', function(){
-            console.log("critical")
-            if (criticalbuttoncounter % 2==0) {
-            $(".project-information-critical").removeClass("opacity-off-start");
-            $(".project-information-critical").removeClass("opacity-on");
-            $(".project-information-critical").addClass("opacity-off");
-            $("#criticalbutton").addClass("buttonminus")
-            $("#criticalbutton").removeClass("buttonplus")
-            console.log('minus')
-            criticalbuttoncounter = criticalbuttoncounter + 1
-            console.log(criticalbuttoncounter)
-            function moveback(){
-                $(".project-information-critical").removeClass("move-up")
-                $(".project-information-critical").addClass("move-back")
-            }
-            setTimeout(moveback,500);  }
-            else {
-            $(".project-information-critical").addClass("opacity-on");
-            $(".project-information-critical").removeClass("opacity-off");
-            $("#criticalbutton").addClass("buttonplus")
-            $("#criticalbutton").removeClass("buttonminus")
-            console.log('plus')
-            criticalbuttoncounter = criticalbuttoncounter + 1
-            console.log(criticalbuttoncounter)
-            
-            function moveup(){
-                $(".project-information-critical").removeClass("move-back")
-                $(".project-information-critical").addClass("move-up")
-            }
-            moveup(); 
-            } 
-            });
-
-        $("#critical").on('click', function(){
-                if (countercritical >= carouselImages.length-1) {
-                    return;
-                }
-                console.log('clicked')
-                carouselSlide.style.transition = "transform 1s ease-in-out";
-                countercritical++;
-                carouselSlide.style.transform= 'translateX(' +(-size * countercritical) + 'px)';
-            });
-            
-        carouselSlide.addEventListener('transitionend', ()=>{
-                if (carouselImages[countercritical].id === 'firstClonecritical') {
-                        carouselSlide.style.transition = "none";
-                        countercritical = carouselImages.length - countercritical;
-                        carouselSlide.style.transform = 'translateX(' +(-size * countercritical) + 'px)';}
-            });
-
-            // groji button // 
 
 
-        var grojibuttoncounter = 1
-        $("#buttongroji").on('click', function(){
-            console.log("groji")
-            if (grojibuttoncounter % 2==0) {
-            $(".project-information-groji").removeClass("opacity-off-start");
-            $(".project-information-groji").removeClass("opacity-on");
-            $(".project-information-groji").addClass("opacity-off");
-            $("#grojibutton").addClass("buttonminus")
-            $("#grojibutton").removeClass("buttonplus")
-            console.log('minus')
-            grojibuttoncounter = grojibuttoncounter + 1
-            console.log(criticalbuttoncounter)
-            function moveback(){
-                $(".project-information-groji").removeClass("move-up")
-                $(".project-information-groji").addClass("move-back")
-            }
-            setTimeout(moveback,500);
-            }
-            else {
-            $(".project-information-groji").addClass("opacity-on");
-            $(".project-information-groji").removeClass("opacity-off");
-            $("#grojibutton").addClass("buttonplus")
-            $("#grojibutton").removeClass("buttonminus")
-            console.log('plus')
-            grojibuttoncounter = grojibuttoncounter + 1
-            console.log(grojibuttoncounter)
-            
-            function moveup(){
-                $(".project-information-groji").removeClass("move-back")
-                $(".project-information-groji").addClass("move-up")
-            }
-            moveup(); 
-            } 
-            });
+// scroll 
 
-        $("#OG").on('click', function(){
+window.addEventListener("scroll", function(event){
+    var studiosectionwidth = $(".flexbox-item-studio").width() + 921 + 7;
+    var clientsectionwidth = $(".flexbox-item-client-container").width() + 921;
+    $(".flexbox-item-transparent-client").height(clientsectionwidth);
+    $(".flexbox-item-transparent").height(studiosectionwidth);
+    var scroll =this.scrollY;
+    console.log(scroll)
+    console.log(studiosectionwidth + "sectionstudiowidth")
+    var scrollable = document.documentElement.scrollHeight - window.innerHeight
+    // const scrolled = window.scrollY;
+    // 11823
+
+    if(scroll < (studiosectionwidth)){
+        scrollable = scrollable + window.innerHeight
+        console.log('when client container is hidden' + 'scrollable is' + scrollable)
+        var left = $(".flexbox-item-studio").css("left")
+        console.log(left + "left")
+        console.log(scroll)
+        $(".flexbox-item-client-container").hide();
+        $(".flexbox-item-studio").show();
+        $(".flexbox-item-studio").css("left" , (- (1*window.scrollY)) + "px");
+    
+    }
+    else {
+        console.log('when client container is shown' + 'scrollable is' + scrollable)
+        $(".flexbox-item-client-container").show();
+        $(".flexbox-item-studio").css("visibilty" , "hidden");
+        $(".flexbox-item-studio").css("left" , (studiosectionwidth));
+        $(".flexbox-item-client-container").css("left" , (- (1*(window.scrollY-(studiosectionwidth + (window.innerHeight/2)))) + "px"));
         
-            if (OGcounter >= carouselImagesOG.length-1) return;
-                carouselSlideOG.style.transition = "transform 1s ease-in-out";
-                OGcounter++;
-                carouselSlideOG.style.transform= 'translateX(' +(-size * OGcounter) + 'px)';
-        });
-
-
-            carouselSlideOG.addEventListener('transitionend', ()=>{
-            if (carouselImagesOG[OGcounter].id === 'firstCloneog') {
-                carouselSlideOG.style.transition = "none";
-                OGcounter = carouselImagesOG.length - OGcounter;
-                carouselSlideOG.style.transform = 'translateX(' +(-size * OGcounter) + 'px)';}
-
-        });
-        
+    }
 
 });
+
+// 3D
 
 
 let scene, camera, renderer;
@@ -247,7 +310,14 @@ function init() {
                         scene.add(gltf.scene);
                         renderer.render(scene,camera);
                         animate();
-             
+                    
+
+                        // mixer = new THREE.AnimationMixer(object);
+                        // var open = mixer.clipAction(fileAnimations[0]);
+                        // open.play();
+
+                    
+                    
                 });
 
                 
